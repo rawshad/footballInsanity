6df9da7676a2d4064f1269ce6e23c94ace7d6e21
rawshad <rawshad@outlook.com>
2022-03-16 18:07:32 +0600

    problem fixed

--- src/Components/TeamContainer/TeamContainer.js
+++ src/Components/TeamContainer/TeamContainer.js
@@ -6,13 +6,14 @@ import TeamCard from '../TeamCard/TeamCard';
 const TeamContainer = () => {
     const [team, setTeam] = useState([]);
   useEffect(() => {
-    fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League')
+    // fetch('https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League')
+    fetch("https://www.thesportsdb.com/api/v1/json/2/search_all_teams.php?l=English%20Premier%20League")
       .then(res => res.json())
       // .then(data => setTeam(data.teams))
       .then(data => setTeam(data.teams))
       .catch(error => console.log('ERROR'))
   }, []);
-  console.log(team);
+  // console.log(team);
   return (
     <section className="container team__container">
       <div className="row">
